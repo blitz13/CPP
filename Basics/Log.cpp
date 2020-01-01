1f541465a64c1f3dd0a260d3526532862791dea2
blitz13 <eizeldelmonte@gmail.com>
2020-01-01 10:29:36 +0800

    Added Log.cpp (Function)

---
+++ Basics/Log.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// IO
+void Write(const char* message)
+{
+	std::cout << message;
+}
+
+void WriteLine(const char* message)
+{
+	std::cout << message << std::endl;
+}
+
+const char*  Read()
+{
+	std::cin;
+}
