803117d461df972313343d03b3718c92f04a51b4
blitz13 <eizeldelmonte@gmail.com>
2020-01-02 14:01:03 +0800

    Conditions and branches

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+
+using std::cin;
 using std::cout;
+using std::endl;
 
 int main()
 {
-    // displays message in the console
-    cout << "Hello World!\n";
+    unsigned short usUserAge = 18;
+    
+    cout << "How old are you?" << endl;
+    cin >> usUserAge;
+
+    if (usUserAge >= 18) {
+        cout << "You can now access mature content." << endl;
+    }
+    else {
+        cout << "You're too young for this." << endl;
+
+        if ((18 - usUserAge) == 1) {
+            cout << "Come back after " << (18 - usUserAge) << " year." << endl;
+        }
+        else {
+            cout << "Come back after " << (18 - usUserAge) << " years." << endl;
+        }
+    }
 }
