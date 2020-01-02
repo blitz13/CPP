8cd101966cf569edc4cca1cf1d5971e8eafdf1fd
blitz13 <eizeldelmonte@gmail.com>
2020-01-02 09:06:19 +0800

    Functions > Return types and Parameters

--- Basics/Basics.cpp
+++ Basics/Basics.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <string>
 
-using std::string;
+//Function with return type and parameters
+float Sum(float a, float b) //parameters are "a" and "b"
+{
+	return a + b; // return type is float
+}
 
-void WriteLine(string);
-string Read();
+//Function with return type only
+float SumFixed()//no parameter
+{
+	return 2 + 2;// return type is float
+}
+
+//Function without return type but with parameter
+void DispalySum(float a, float b)
+{
+	float result = Sum(a, b);// "Sum()" function with return type
+	std::cout << result << std::endl;// Displays the sum of two numbers
+}
+
+//Function without return type and parameter
+void Instructions()
+{
+	std::cout << "Copy and Paste" << std::endl;
+}
 
 int main()
 {
-    WriteLine("Hello " + Read());
+	DispalySum(2, 2);
 }
