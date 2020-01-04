cc28e21d43b76f9d6ca3420a4fa67854629984be
blitz13 <eizeldelmonte@gmail.com>
2020-01-04 20:45:13 +0800

    for loop

--- Basics/source/Basics.cpp
+++ Basics/source/Basics.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
 int main()
 {
-     // display sizes of variable types
-    cout << "The size of a CHAR is " << sizeof(char) << " byte" << endl;
-    cout << "The size of a SHORT INT is " << sizeof(short) << " bytes" << endl;
-    cout << "The size of a INT is " << sizeof(int) << " bytes" << endl;
-    cout << "The size of a LONG INT is " << sizeof(long) << " bytes" << endl;
-    cout << "The size of a LONg LONG INT is " << sizeof(long long) << " bytes" << endl;
-    cout << "The size of a FLOAT is " << sizeof(float) << " bytes" << endl;
-    cout << "The size of a DOUBLE is " << sizeof(double) << " bytes" << endl;
-    cout << "The size of a LONG DOUBLE is " << sizeof(long double) << " bytes" << endl;
-    cin.get();
+	//for loops
+	for (int i = 0; i < 2; i++) {
+		std::cout << "Hello" << std::endl;
+	}
+
+	//or
+	/*int i = 0;
+	for (;i < 2;)
+	{
+		std::cout << "Hello" << std::endl;
+		i++;
+	}*/
+
+
 }
