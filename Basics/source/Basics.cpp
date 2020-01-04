739a676035701a67a56085fb3108f8e0f3037898
blitz13 <eizeldelmonte@gmail.com>
2020-01-05 07:15:05 +0800

    Return statement

--- Basics/source/Basics.cpp
+++ Basics/source/Basics.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 
+// "Return"
+bool IsAdult()
+{
+	int age = 18;
+	std::cin >> age;
+	if (age >= 18) {
+		std::cout << "True..." << std::endl;
+		return true;
+	}
+	else {
+		std::cout << "False..." << std::endl;
+		return false;
+	}
+}
+
 int main()
 {
-	for (int i = 0; i < 4; i++)
-	{
-		// "Continue"
-		/*if (i % 2)		
-			continue;
-		std::cout << i << " ";
-		std::cout << "Hello" << std::endl;*/
+	//for (int i = 0; i < 4; i++)
+	//{
+	//	// "Continue"
+	//	/*if (i % 2)		
+	//		continue;
+	//	std::cout << i << " ";
+	//	std::cout << "Hello" << std::endl;*/
 
-		// "Break"
-		if (i % 2)
-			break;
-		std::cout << i << " ";
-		std::cout << "Hello" << std::endl;
-	}
+	//	// "Break"
+	//	/*if (i % 2)
+	//		break;
+	//	std::cout << i << " ";
+	//	std::cout << "Hello" << std::endl;*/
+
+	//}
+
+	// "Return"
+	while (!IsAdult())
+	{
+		IsAdult();
+	} 
 }
