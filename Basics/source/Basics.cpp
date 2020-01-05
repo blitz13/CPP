478c2c4b1414967774185d047c84be44d303fea9
blitz13 <eizeldelmonte@gmail.com>
2020-01-05 11:05:46 +0800

    Pointer over Reference

--- Basics/source/Basics.cpp
+++ Basics/source/Basics.cpp
@@ -10,21 +10,35 @@
 //    }
 //}
 
-void AgeStatus(int& age) 
-{
-    if (age >= 18) {
-        std::cout << "You are an adult..." << std::endl;
-    }
-    else {
-        std::cout << "You are a minor..." << std::endl;
-    }
-}
+//void AgeStatus(int& age) 
+//{
+//    if (age >= 18) {
+//        std::cout << "You are an adult..." << std::endl;
+//    }
+//    else {
+//        std::cout << "You are a minor..." << std::endl;
+//    }
+//}
 
 int main()
 {
-    int iUserAge = 18;
-    std::cout << "How old are you?" << std::endl;
-    std::cin >> iUserAge;
-    //AgeStatus(&iUserAge); // Using pointer
-    AgeStatus(iUserAge); // Reference
+    //int iUserAge = 18;
+    //std::cout << "How old are you?" << std::endl;
+    //std::cin >> iUserAge;
+    ////AgeStatus(&iUserAge); // Using pointer
+    //AgeStatus(iUserAge); // Reference
+
+    // Pointer over Reference ------------------------------------------------------------
+    int x = 0, y = 0;
+   
+    // Reference
+    int& rf = x;
+    rf = y; // ! NOT PPSSIBLE... cannot use one reference(rf) with two variables(x and y)
+    
+    // Pointer
+    int* ptr = &x; // point to "x" address
+    *ptr = 1; // change "x" value to 1
+    ptr = &y; // point to "y" address
+    *ptr = 2; // change "y" value to 2
+
 }
